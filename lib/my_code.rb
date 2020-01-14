65d4acd5b112c5fe53918775eaa2d936e7c7f130
John Reiner <johnreiner1990@gmail.com>
2020-01-14 23:14:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ def map(array)
   end
   new_array
 end
+
+def reduce(array)
+  total = 0
+  count = 0
+  while count < array.length
+    total += (yield(array[count]))
+    count += 1
+  end
+  total
+end
